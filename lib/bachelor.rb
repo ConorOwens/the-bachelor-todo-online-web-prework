13949e2f4690906fb9abe2ba468d4bfb14113f64
Conor Owens <conorj.owens@gmail.com>
2019-04-13 22:01:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,7 +22,13 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, info|
+    if info["hometown"] == hometown
+      count += 1 
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
