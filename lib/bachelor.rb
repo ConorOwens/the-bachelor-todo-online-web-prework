488689592294a3ac64b71ce4024d6c08647224d0
Conor Owens <conorj.owens@gmail.com>
2019-04-13 21:44:07 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,7 +4,7 @@ def get_first_name_of_season_winner(data, season)
   # code here
   data[season].each do |info|
     if info["status"] = "Winner"
-      return info["name"]
+      return info["name"].split(" ")[0]
         end
       end
     end
